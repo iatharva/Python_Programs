76a4bab8d93c56e8b61690939e33c463118b1758
Atharva Ikhar <atharva464@gmail.com>
2021-10-20 23:14:02 +0530

    Create Q3.py

---
+++ SessionI/Q3.py
@@ -0,0 +1,7 @@
+#Write a program to read two numbers and find the sum of their cubes.
+
+#Accepting two numbers from the user
+number1 = int(input("Enter a number : "));
+number2 = int(input("Enter another number : "));
+#Printing sum of the cubes of the two numbers
+print("The sum of the cubes of the two numbers is : ", number1**3 + number2**3)
